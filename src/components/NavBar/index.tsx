61e941157b65fd3eb737ad3e22d6828e15f1e648
Abhishek R <abhiisheek@hotmail.com>
2025-03-12 21:35:55 +0530

    (add): App logo to nav header

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,15 +1,24 @@
 import React from "react";
-import { Layout, Typography, Divider, Flex } from "antd";
+import { Layout, Typography, Divider, Flex, Row, Col } from "antd";
 import { Link } from "react-router-dom";
+
+import appIcon from "@/images/app-icon.jpg";
 
 const Navbar: React.FC = () => {
   return (
     <Layout.Sider width={100} className='!bg-white'>
-      <Typography.Title className='!text-[20px] flex justify-center min-h-[64px] items-center !mb-0'>
-        ZeroDash
-      </Typography.Title>
-      <Divider className="!m-0" />
-      <Flex gap='large' vertical justify='center' className="!mx-[8px]">
+      <Row gutter={4} align='middle' justify='center'>
+        <Col>
+          <img src={appIcon} alt='Logo' className="w-[48px] h-[48px] mt-[4px]" />
+        </Col>
+        <Col>
+          <Typography.Text strong>
+            ZeroDash
+          </Typography.Text>
+        </Col>
+      </Row>
+      <Divider className='!my-[8px] !border-[#001529]' />
+      <Flex gap='large' vertical justify='center' className='!mx-[8px]'>
         <Link to='/projects'>Projects</Link>
         <Link to='/dashboards'>Dashboards</Link>
       </Flex>
